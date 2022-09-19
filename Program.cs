cc9c7c7e90c61541341d4ca5fbd848c46eac6675
Dmitriy <simpa-baki@mail.ru>
2022-09-20 01:31:22 +0300

    fillfirst arr

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int FillFirstArray(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine("Введите элемент массива: ");
+        arr[i] = Console.ReadLine();
+        if (arr[i].Length <= 3) count++;
+    }
+    return count;
+}

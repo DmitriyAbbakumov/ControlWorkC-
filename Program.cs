26bb85a5e7987da16358cf8d5901c2f9d5dab206
Dmitriy <simpa-baki@mail.ru>
2022-09-20 01:32:23 +0300

    fillsecond arr

--- Program.cs
+++ Program.cs
@@ -9,3 +9,15 @@
     }
     return count;
 }
+void FillSecondArray(string[] arr1, string[] arr2)
+{
+    int j = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+        if (arr1[i].Length <= 3)
+        {
+            arr2[j] = arr1[i];
+            j++;
+        }
+    }
+}
